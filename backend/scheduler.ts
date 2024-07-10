5dc42a1d28102c995d52431801e9b64739f3a6be
moayaan1911 <ayaangames@gmail.com>
2024-07-10 23:20:06 +0530

    final deploy

--- backend/scheduler.ts
+++ backend/scheduler.ts
@@ -5,19 +5,10 @@ import { storeGasPrice } from './services/gas.service.js';
 import logger from './utils/logger.js';
 
 const startScheduler = () => {
-  // Run every 30 seconds for testing
-  cron.schedule('*/30 * * * * *', async () => {
-    logger.info(
-      'Running scheduled task to fetch and store gas price (30-second interval for testing)'
-    );
+  cron.schedule('*/30 * * * *', async () => {
+    logger.info('Running scheduled task to fetch and store gas price');
     await storeGasPrice();
   });
-
-  // 30-minute interval (commented out for now)
-  // cron.schedule('*/30 * * * *', async () => {
-  //   logger.info('Running scheduled task to fetch and store gas price');
-  //   await storeGasPrice();
-  // });
 
   logger.info('Scheduler started');
 };
